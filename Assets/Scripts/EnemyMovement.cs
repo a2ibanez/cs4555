c9b05af4f5a26afe07aafe33eb745f00c28b1954
Hawo28709 <apaname3@calstatela.edu>
2026-03-25 21:00:26 -0700

    Enemy
    Enemy model + basic run animation

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,24 +5,51 @@ public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent agent;
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player != null){
-            agent.SetDestination(player.position);
+        if (player != null)
+    {
+        agent.SetDestination(player.position);
+
+        
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0f;
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                lookRotation,
+                10f * Time.deltaTime
+            );
         }
+    }
+
+    bool isMoving = !agent.isStopped
+        && agent.velocity.magnitude > 0.1f
+        && agent.remainingDistance > agent.stoppingDistance;
+
+    animator.SetBool("isRunning", isMoving);
+
     }
 
     private void OnCollisionEnter(Collision collision){
         if(collision.gameObject.CompareTag("player")){
             print("hit");
             agent.isStopped = true;
+            animator.SetBool("isRunning", false);
         }
         
     }
